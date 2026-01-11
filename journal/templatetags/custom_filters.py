2fe6ea3a8ac1cd7dd15aef43dfd0c1f6610857ef
Dave Perkins <dperkins@colgate.edu>
2026-01-11 09:22:26 -0500

    Add flatten filter to custom_filters for calendar template

--- journal/templatetags/custom_filters.py
+++ journal/templatetags/custom_filters.py
@@ -8,3 +8,15 @@ def get_item(dictionary, key):
     if dictionary is None:
         return []
     return dictionary.get(key, [])
+
+@register.filter
+def flatten(value):
+    """Flatten a list of lists into a single list."""
+    result = []
+    if value:
+        for item in value:
+            if isinstance(item, (list, tuple)):
+                result.extend(item)
+            else:
+                result.append(item)
+    return result
